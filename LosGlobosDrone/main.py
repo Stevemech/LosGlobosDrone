13b9eb4432dc3562c226742429206c284ce205fd
jypan2007 <jypan2007@gmail.com>
2023-05-04 23:01:48 -0700

    check-in base code

---
+++ LosGlobosDrone/main.py
@@ -0,0 +1,17 @@
+
+
+from algo import *
+from in_out import *
+
+def main(filename):
+    # Example usage
+    waypoints = readWaypointFile(filename)
+    print(waypoints)
+
+    # Determine path
+    bestPath = determinePath(waypoints)
+
+    print("Best Path:", bestPath)
+
+if __name__ == '__main__':
+	main("waypoint_1.txt")
